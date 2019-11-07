20f57ee8ab3643c0550bebe54c1873f1e37b281b
ReaganShor <47014697+ReaganShor@users.noreply.github.com>
2019-11-07 15:16:41 -0800

    Files for the fade effect (still in progress)

---
+++ Assets/Scripts/DemoFadeEffect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoFadeEffect : MonoBehaviour
+{
+
+    //Variables 
+    public Material mat;
+    public GameObject DemoBird;
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    private void Start()
+    {
+        DemoBird.GetComponent<Renderer>().material = mat;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("A down");
+
+            if (gameObject.tag == "DemoBird")
+            {
+                Color color = mat.color;
+                color.a = 1f;
+                print("a wild animal appeared");
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            print("S Down");
+            if (gameObject.tag == "DemoBird")
+            {
+                Color color = mat.color;
+                color.a = 0f;
+                print("it ran away!");
+            }
+        }
+    }
+}

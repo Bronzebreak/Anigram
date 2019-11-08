870872b0fdfea0696b85106e8999f1a6bee97e06
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-11-07 17:17:52 -0800

    particleSystemsmallUpdate

--- Assets/Andrew_Test_Assets/WindControll.cs
+++ Assets/Andrew_Test_Assets/WindControll.cs
@@ -8,7 +8,8 @@ public class WindControll : MonoBehaviour
     public FollowPath followPathRef;
     public FollowPath followPathRef2;
     public GameObject spawnPoint;
-    float time ;
+    public float time1;
+    public float time2;
     public GameObject particlePrefab1;
     public GameObject particleInstance1;
     public GameObject particlePrefab2;
@@ -34,18 +35,18 @@ public class WindControll : MonoBehaviour
     IEnumerator Timer()
     {
         while (true){
-            time =2.0f;
+            time1 = 2.0f;
             yield return new WaitForSeconds(0.3f);
             particleInstance1 = Instantiate(particlePrefab1, spawnPoint.transform.position, Quaternion.identity);
             followPathRef = particleInstance1.GetComponentInChildren<FollowPath>();
-            followPathRef.TimeTotal = time;  
-            yield return new WaitForSeconds(time+0.6f);
+            followPathRef.TimeTotal = time1;  
+            yield return new WaitForSeconds(time1 + 0.6f);
             Destroy(particleInstance1);
             particleInstance2 = Instantiate(particlePrefab2, spawnPoint.transform.position, Quaternion.identity);
             followPathRef2 = particleInstance2.GetComponentInChildren<FollowPath>();
-            time = 4.0f;
-            followPathRef2.TimeTotal = time;
-            yield return new WaitForSeconds(time+0.5f);
+            time2 = 4.0f;
+            followPathRef2.TimeTotal = time2;
+            yield return new WaitForSeconds(time2 + 0.5f);
             Destroy(particleInstance2);
             print("3");
         }

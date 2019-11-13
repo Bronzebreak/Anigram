4924213bc7a00cc6166ea7e9582fad38d8130eda
ReaganShor <47014697+ReaganShor@users.noreply.github.com>
2019-11-12 21:20:25 -0800

    Updates to dust mat, ani, sprite, particles,

--- Assets/Scripts/DemoFadeEffect.cs
+++ Assets/Scripts/DemoFadeEffect.cs
@@ -8,20 +8,22 @@ public class DemoFadeEffect : MonoBehaviour
     //Variables 
     public Material mat;
     public GameObject DemoBird;
+
     float alphaLevel = 1;
-    float fadetime;
+    bool animalHidden;
 
-//---------------------------------------------------------------------------------------------------------------------
+    public ParticleSystem DustParticle;
+    public ParticleSystem BlownAwayParticle;
 
-    //Upon starting the game, get the gameobject and set it's material as 'mat'
+//-------------------------------------------------------------------------------------------------------------------------
+    //Upon starting the game, get the gameobject and set it's material as 'mat', and hid the main animal.
     private void Start()
     {
-        DemoBird.GetComponent<Renderer>().material = mat;
+        DemoBird.GetComponent<MeshRenderer>().material = mat;
+        animalHidden = true;
     }
 
-//---------------------------------------------------------------------------------------------------------------------
-
-
+//-------------------------------------------------------------------------------------------------------------------------
     void Update()
     {
         // Then, everytime the A key is pressed, set color to the mat color and have the alpha turned onto 1.
@@ -29,16 +31,52 @@ public class DemoFadeEffect : MonoBehaviour
         {
             print("A down");
             Color color = mat.color;
-            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, 1); //f - 0.1f * (Time.deltaTime)
+            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, 1);
+            animalHidden = false;
             print("a wild animal appeared");
+            print("made it!");
         }
 
-        // Then, everytime the S key is pressed, set color to the mat color and have the alpha turned onto 0.
+//-------------------------------------------------------------------------------------------------------------------------
+        // Then, everytime the S key is pressed, set color to the mat color and have the alpha turned onto 0. When object is hidden play Blown away particle
         if (Input.GetKeyDown(KeyCode.S))
         {
+            //fadeIn();
             print("S Down");
-            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, 0); //+ 0.1f / (Time.deltaTime)
+            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, 0);
             print("it ran away!");
+            animalHidden = true;
+            BlownAwayParticle.Play();
+            print("made it2!");
+        }
+//-------------------------------------------------------------------------------------------------------------------------
+
+        // if the animal is hidden, have the particles stop.
+        if (animalHidden == true)
+        {
+            DustParticle.Stop();
+        }
+        //Otherwise, in the animal is not hidden, play particle effect.
+        else if (animalHidden == false)
+        {
+            DustParticle.Play();
         }
     }
+//-------------------------------------------------------------------------------------------------------------------------
+
+
+    /*
+        void fadeIn()
+        {
+            while (mat.color.a > 0)
+            {
+                Color newColor = mat.color;
+                newColor.a -= Time.deltaTime;
+                mat.color = newColor;
+            }
+        }
+    */
+
+
+
 }

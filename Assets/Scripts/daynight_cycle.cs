7b48c5e59c6f061748ce6dec00a4dcd9fec3fbdc
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-11-21 14:25:17 -0800

    Merging scripts

--- Assets/Scripts/daynight_cycle.cs
+++ Assets/Scripts/daynight_cycle.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class daynight_cycle : MonoBehaviour
 {
+    //Wind Particle Variables
+    public FollowPath followPathRef; //reference to script in first particle
+    public FollowPath followPathRef2; //reference to script in second particle
+    public GameObject spawnPoint; //Spawn Point of the wind
+    float time1 = 2.3f; // initial time of the inhale cycle
+    float time2 = 4.3f; // initial time of exhale cycle
+    float timeDelay = 0.3f; // delay between inhale and exhale
+    float timeNow; // time since the start of the program
+    public GameObject particlePrefab1; // reference to prefabe
+    public GameObject particleInstance1; // instantiated object
+    public GameObject particlePrefab2;
+    public GameObject particleInstance2;
+
+    // day night cycle variables
     public Light sun;
     public float secondsInFullDay = 120f;
     [Range(0, 1)]
@@ -34,15 +48,24 @@ public class daynight_cycle : MonoBehaviour
     public Gradient nightDayColor;
     Material skyMat;
     */
-
+    public void FullCircle()
+    {
+        StartCoroutine(Timer()); // starts inhale exghale cycle
+    }
     void Start()
     {
         sunInitialIntensity = sun.intensity;
         //InvokeRepeating("UpdateCycle", updateRateInSeconds, updateRateInSeconds);
+        FullCircle(); // calls the function at the begining of the run
     }
 
     void Update()
     {
+
+        BoxCollider[] bc = GetComponentsInChildren<BoxCollider>();
+        timeNow = Time.realtimeSinceStartup; // sets timne to the runtime of the programm
+
+
         UpdateSun();
         //currentTimeOfDay += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
         currentTimeOfDay =( 0.45f + (Time.realtimeSinceStartup/secondsInFullDay) ) * timeMultiplier;
@@ -77,39 +100,66 @@ public class daynight_cycle : MonoBehaviour
         sun.intensity = sunInitialIntensity * intensityMultiplier;
     }
 
-   // void AdjustSunColor()
-   // {
-     //   sun.color = sunColor.Evaluate(sun.intensity);
+    // void AdjustSunColor()
+    // {
+    //   sun.color = sunColor.Evaluate(sun.intensity);
     //}
 
-       /*
-    void UpdateFX()
+    /*
+ void UpdateFX()
+ {
+     float tRange = 1 - minPoint;
+     float dot = Mathf.Clamp01((Vector3.Dot(sun.transform.forward, Vector3.down) - minPoint) / tRange);
+     float i = ((maxIntensity - minIntensity) * dot) + minIntensity;
+     sun.intensity = i;
+     if (moon != null)
+         moon.intensity = 1 - i;
+
+     i = ((maxBounceIntensity - minBounceIntensity) * dot) + minBounceIntensity;
+     sun.bounceIntensity = i;
+
+     tRange = 1 - minAmbientPoint;
+     dot = Mathf.Clamp01((Vector3.Dot(sun.transform.forward, Vector3.down) - minAmbientPoint) / tRange);
+     i = ((maxAmbient - minAmbient) * dot) + minAmbient;
+     RenderSettings.ambientIntensity = i;
+
+     sun.color = nightDayColor.Evaluate(dot);
+     RenderSettings.ambientLight = sun.color;
+
+     RenderSettings.fogColor = nightDayFogColor.Evaluate(dot);
+     RenderSettings.fogDensity = fogDensityCurve.Evaluate(dot) * fogScale;
+
+     i = ((dayAtmosphereThickness - nightAtmosphereThickness) * dot) + nightAtmosphereThickness;
+     skyMat.SetFloat("_AtmosphereThickness", i);
+     skyMat.SetFloat("_Exposure", i * exposureMultiplier);
+ }
+
+ */
+
+    IEnumerator Timer()
     {
-        float tRange = 1 - minPoint;
-        float dot = Mathf.Clamp01((Vector3.Dot(sun.transform.forward, Vector3.down) - minPoint) / tRange);
-        float i = ((maxIntensity - minIntensity) * dot) + minIntensity;
-        sun.intensity = i;
-        if (moon != null)
-            moon.intensity = 1 - i;
+        while (true)
+        {
+            time1 = 2 + (1 * (timeNow / (secondsInFullDay/4)));// updates the time for the graduale increase of the inhale cycle
+            time2 = 4 + (3 * (timeNow / (secondsInFullDay / 4)));// updates the time for the graduale increase of the exhale cycle
+            timeDelay = 0.3f + (0.3f * (timeNow / (secondsInFullDay / 4)));// updates delay between cycles
 
-        i = ((maxBounceIntensity - minBounceIntensity) * dot) + minBounceIntensity;
-        sun.bounceIntensity = i;
-
-        tRange = 1 - minAmbientPoint;
-        dot = Mathf.Clamp01((Vector3.Dot(sun.transform.forward, Vector3.down) - minAmbientPoint) / tRange);
-        i = ((maxAmbient - minAmbient) * dot) + minAmbient;
-        RenderSettings.ambientIntensity = i;
-
-        sun.color = nightDayColor.Evaluate(dot);
-        RenderSettings.ambientLight = sun.color;
-
-        RenderSettings.fogColor = nightDayFogColor.Evaluate(dot);
-        RenderSettings.fogDensity = fogDensityCurve.Evaluate(dot) * fogScale;
-
-        i = ((dayAtmosphereThickness - nightAtmosphereThickness) * dot) + nightAtmosphereThickness;
-        skyMat.SetFloat("_AtmosphereThickness", i);
-        skyMat.SetFloat("_Exposure", i * exposureMultiplier);
+            particleInstance1 = Instantiate(particlePrefab1, spawnPoint.transform.position, Quaternion.identity);
+            followPathRef = particleInstance1.GetComponentInChildren<FollowPath>();
+            followPathRef.TimeTotal = time1;
+            particleInstance1.SetActive(false);
+            particleInstance2 = Instantiate(particlePrefab2, spawnPoint.transform.position, Quaternion.identity);
+            followPathRef2 = particleInstance2.GetComponentInChildren<FollowPath>();
+            followPathRef2.TimeTotal = time2;
+            particleInstance2.SetActive(false);
+            yield return new WaitForSeconds(timeDelay);
+            particleInstance1.SetActive(true);
+            yield return new WaitForSeconds(time1 + timeDelay);
+            Destroy(particleInstance1);
+            particleInstance2.SetActive(true);
+            yield return new WaitForSeconds(time2 + timeDelay);
+            particleInstance2.SetActive(false);
+            Destroy(particleInstance2);
+        }
     }
-
-    */
 }

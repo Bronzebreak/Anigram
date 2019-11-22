31e92240ac7679be8ab528b07008ba11281e37f2
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-11-21 17:23:30 -0800

    no message

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -8,14 +8,20 @@ public class Animal : MonoBehaviour
     public AudioSource mySound;
     public GameObject spawnPoint;
 
+    //On initialization...
     private void Start()
     {
+        //...encapsulate my audio source as a variable...
         mySound = gameObject.GetComponent<AudioSource>();
+
+        //...and set the sound to play of my Audio Source based off of the publicly set variable Audio Clip variable.
         mySound.clip = soundToPlay;
     }
 
+    //On trigger...
     public void PlaySound()
     {
+        //...play my publicly set sound.
         mySound.Play();
     }
 }
--- Assets/Scripts/Overlord.cs
+++ Assets/Scripts/Overlord.cs
@@ -31,20 +31,31 @@ public class Overlord : MonoBehaviour
 
     #endregion
 
-    // Start is called before the first frame update
+    //On itialization...
     void Start()
     {
+        //...encapsulate the GazeInput to allow for modification to the hoverDelay and hoverDuration components.
         gazeRef = avatarRef.GetComponent<GazeInput>();
-        InvokeRepeating("Spawn", startDelay, repeatDelay);
+
+        //...encapsulate the Camera used (in PIE sessions, at least) to allow for the LookAt() function to be ran.
         head = centerEye.GetComponent<Camera>();
+
+        //...run Spawn after startDelay seconds, repeating every repeatDelay seconds.
+        InvokeRepeating("Spawn", startDelay, repeatDelay);
     }
 
-    // Update is called once per frame
+    //Once a frame...
     void Update()
     {
+        //...call function to update the values of the GazeInput.
         UpdateGazePointerProperties(breathDelay, inhaleTime);
+
+        //...reorient the phone to face the encapsulated camera,
         phone.transform.LookAt(head.transform);
+        //then orient the camera in world space to always appear upright.
         phone.transform.eulerAngles = new Vector3(phone.transform.rotation.eulerAngles.x + 90, phone.transform.rotation.eulerAngles.y, phone.transform.rotation.eulerAngles.z);
+
+        #region Pseudo-Code
         /*
         Get animController
         animControllerRef.inhaleDelay = inhaleDelay; repeat for time/exhales.
@@ -53,29 +64,38 @@ public class Overlord : MonoBehaviour
         sun delta start-end time == breathing delta inhale start-end time. 
         endbreathDelay = .6f, inhaleTime = 3f, exhaletime = 7f
         */
+        #endregion
     }
-    
+
+    //When called...
     public void UpdateGazePointerProperties(float delay, float duration)
     {
+        //...set encapsulated GazeInput component based off of passed in variables.
         gazeRef.HoverDelay = delay;
         gazeRef.HoverDuration = duration;
 
+        //Code provided by Tin of liminalVR in order to allow for GazeInput properties to be accessed.
         var type = gazeRef.GetType();
         var applyPointerPropertiesMethod = type.GetMethod("ApplyTimedPointerProperties", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
         if (applyPointerPropertiesMethod != null)
             applyPointerPropertiesMethod.Invoke(gazeRef, null);
     }
 
+
+    //When called...
     void Spawn()
-    { 
+    {
+        //...if the maximum amount of spawns is not reached...
         if (spawnCount < spawnMax)
         {
+            //...return a random object from the animals list,
             int index = Random.Range(0, animals.Length);
             GameObject toSpawn = animals[index];
+            //and create local variables for the spawn actor and string used in the FindAll call.
             GameObject spawnLocation;
             string zone;
-
-            print(index.ToString());
+            //Then, determine which biome is used based off of which animal is selected,
+            #region Determine Biome
             if (index < 2)
             {
                 zone = "Hill";
@@ -95,27 +115,29 @@ public class Overlord : MonoBehaviour
             {
                 zone = "Pond";
             }
-            print(zone);
-
+            #endregion
+            //find all potential spawn points in the biome,
             results = spawnPoints.FindAll(
             delegate(GameObject point)
             {
             return point.name.Contains(zone);
             }
             );
-
+            //instantiate the animal randomly at one of the points,
             int spawnIndex = Random.Range(0, results.Count);
             spawnLocation = results[spawnIndex];
             GameObject spawned = Instantiate(toSpawn, spawnLocation.transform.position, spawnLocation.transform.rotation);
-
+            //if the animal is flying, modify its spawn to above ground,
             if (index == 0 || index == 2)
             {
                 spawned.transform.position = spawned.transform.position + new Vector3 (0, 2, 0);
             }
+            //instantiate the publicly set paricle at the location of the spawned creature,
             ParticleSystem spawnedParticle = Instantiate(spawnParticle, spawnLocation.transform.position, spawnLocation.transform.rotation);
-
+            //then publicly set the spawnPoint and sound in the instantiated animal's code,
             spawned.GetComponent<Animal>().spawnPoint = results[spawnIndex];
             spawned.GetComponent<Animal>().soundToPlay = sounds[index];
+            //before finally removing the used location as a potential spawn point and increasing the total count of spawned creatures.
             spawnPoints.Remove(results[spawnIndex]);
             spawnCount++;    
         }

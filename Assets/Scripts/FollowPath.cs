c5852b75f8a0d5a65b795a4da76036746514b645
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-12-05 17:36:45 -0800

    no message

--- Assets/Scripts/FollowPath.cs
+++ Assets/Scripts/FollowPath.cs
@@ -4,17 +4,13 @@ using System.Collections.Generic;
 
 public class FollowPath : MonoBehaviour
 {
-    #region Enums
     public enum MovementType  //Type of Movement
     {
         MoveTowards,
         LerpTowards
     }
-    #endregion //Enums
 
-    #region Public Variables
-    
-
+    #region Variables
     public MovementType Type = MovementType.MoveTowards; // Movement type used
     public MovementPath MyPath; // Reference to Movement Path Used
     private float Speed1 ; // Speed object is moving
@@ -23,19 +19,40 @@ public class FollowPath : MonoBehaviour
     float totalDistance1 = 0.0f;
     float totalDistance2 = 0.0f;
     public float MaxDistanceToGoal = .1f; // How close does it have to be to the point to be considered at point
-    #endregion //Public Variables
-
-    #region Private Variables
     private IEnumerator<Transform> pointInPath; //Used to reference points returned from MyPath.GetNextPathPoint
-    #endregion //Private Variables
+    #endregion
 
     // (Unity Named Methods)
     #region Main Methods
-    
+           
+    public void Awake()
+    {
+        for (int points =0; points < 12; points++ )
+        {
+            float distance = Vector3.Distance(MyPath.PathSequence[points].position, MyPath.PathSequence[points + 1].position);//gets the distance between two points next5 to each other in array.
+            totalDistance1 = totalDistance1 + distance;// adds the distance between two points to total distance 
+        }
+
+        for (int points =12; points < MyPath.PathSequence.Length-1; points++ )
+        {
+            float distance = Vector3.Distance(MyPath.PathSequence[points].position, MyPath.PathSequence[points + 1].position);//gets the distance between two points next5 to each other in array.
+            totalDistance2 = totalDistance2 + distance;// adds the distance between two points to total distance 
+        }
+    }
+     
+    public void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Follow();
+    }
+
     public void Follow()
     {
-
-
         //Make sure there is a path assigned
         if (MyPath == null)
         {
@@ -46,6 +63,7 @@ public class FollowPath : MonoBehaviour
         //Sets up a reference to an instance of the coroutine GetNextPathPoint
         pointInPath = MyPath.GetNextPathPoint();
         Debug.Log(pointInPath.Current);
+
         //Get the next point in the path to move to (Gets the Default 1st value)
         pointInPath.MoveNext();
         Debug.Log(pointInPath.Current);
@@ -59,36 +77,11 @@ public class FollowPath : MonoBehaviour
 
         //Set the position of this object to the position of our starting point
         transform.position = pointInPath.Current.position;
-        
-    }
-
-    public void Awake()
-    {
-        for (int points =0; points < 12; points++ )
-        {
-            float distance = Vector3.Distance(MyPath.PathSequence[points].position, MyPath.PathSequence[points + 1].position);//gets the distance between two points next5 to each other in array.
-            totalDistance1 = totalDistance1 + distance;// adds the distance between two points to total distance 
-        }
-        for (int points =12; points < MyPath.PathSequence.Length-1; points++ )
-        {
-            float distance = Vector3.Distance(MyPath.PathSequence[points].position, MyPath.PathSequence[points + 1].position);//gets the distance between two points next5 to each other in array.
-            totalDistance2 = totalDistance2 + distance;// adds the distance between two points to total distance 
-        }
-        
-        
-    }
-     
-    public void Start()
-    {
-        StartCoroutine(Wait());
-        
     }
 
     //Update is called by Unity every frame
     public void Update()
     {
-
-        //print(TimeTotal);
         //Validate there is a path with a point in it
         if (pointInPath == null || pointInPath.Current == null)
         {
@@ -128,33 +121,6 @@ public class FollowPath : MonoBehaviour
         {
             pointInPath.MoveNext(); //Get next point in MovementPath
         }
-        //The version below uses Vector3.Distance same as Vector3.Magnitude which includes (square root)
-        /*
-        var distanceSquared = Vector3.Distance(transform.position, pointInPath.Current.position);
-        if (distanceSquared < MaxDistanceToGoal) //If you are close enough
-        {
-            pointInPath.MoveNext(); //Get next point in MovementPath
-        }
-        */
-    }
+     }
     #endregion //Main Methods
-
-    //(Custom Named Methods)
-    #region Utility Methods 
-
-    #endregion //Utility Methods
-
-    //Coroutines run parallel to other fucntions
-    #region Coroutines
-
-    IEnumerator Wait()
-    {
-        
-        yield return new WaitForSeconds(0.3f);
-        Follow();
-        
-        
-        
-    }
-    #endregion //Coroutines
 }

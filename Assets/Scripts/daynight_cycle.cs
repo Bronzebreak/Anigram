bb883bd7ae984ccb662c1dccb59f3118a0c065ef
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-12-06 16:54:18 -0800

    Created LeafMover script and applied it to DerekTest.

--- Assets/Scripts/daynight_cycle.cs
+++ Assets/Scripts/daynight_cycle.cs
@@ -6,25 +6,18 @@ public class daynight_cycle : MonoBehaviour
 {
     //Wind Particle Variables
     public FollowPath followPathRef; //reference to script in first particle
-    //public FollowPath followPathRef2; //reference to script in second particle
-    //public FollowPath followPathRef3; //reference to script in second particle
     public GameObject inhaleSpawnPoint;
     public GameObject exhaleSpawnPoint;//Spawn Point of the wind
-    float time1 = 2.3f; // initial time of the inhale cycle
-    float time2 = 4.3f; // initial time of exhale cycle
-    float timeDelay = 0.3f; // delay between inhale and exhale
-    float timeNow; // time since the start of the program
+    public float inhaleTime = 2.3f; // initial time of the inhale cycle
+    public float exhaleTime = 4.3f; // initial time of exhale cycle
+    public float breathDelay = 0.3f; // delay between inhale and exhale
+    float timeSinceLaunch; // time since the start of the program
     public GameObject particlePrefab1; // reference to prefabe
     public GameObject particleInstance1; // instantiated object
-   /* public GameObject particlePrefab2;
-    public GameObject particleInstance2;
-    
-    public GameObject particlePrefab3;
-    public GameObject particleInstance3;*/
 
     // day night cycle variables
     public Light sun;
-    public float secondsInFullDay = 120f;
+    public float secondsInFullDay = 1200f;
     [Range(0, 1)]
     public float currentTimeOfDay = 0;
     [HideInInspector]
@@ -32,35 +25,8 @@ public class daynight_cycle : MonoBehaviour
     float sunInitialIntensity;
     public Light moon;
 
-    /*
-    public Color startColor;
-    public Color lightorangeColor;
-    public Color orangeColor;
-    public Color redColor;
-    public Color pinkColor;
-    public Color endColor;
-    */
     public Gradient sunset;
 
-    //public float updateRateInSeconds = 5f;
-    /*
-    public float maxIntensity = 2f;
-    public float minIntensity = 0f;
-    public float minPoint = -0.2f;
-    public float maxBounceIntensity = 1.0f;
-    public float minBounceIntensity = 0.5f;
-    public float maxAmbient = 1f;
-    public float minAmbient = 0f;
-    public float minAmbientPoint = -0.2f;
-    public Gradient nightDayFogColor;
-    public float fogScale = 1f;
-    public float exposureMultiplier = 1f;
-    public float dayAtmosphereThickness = 0.4f;
-    public float nightAtmosphereThickness = 0.87f;
-    public AnimationCurve fogDensityCurve;
-    public Gradient nightDayColor;
-    Material skyMat;
-    */
     public void FullCircle()
     {
         StartCoroutine(Timer()); // starts inhale exghale cycle
@@ -68,22 +34,14 @@ public class daynight_cycle : MonoBehaviour
     void Start()
     {
         sunInitialIntensity = sun.intensity;
-        //InvokeRepeating("UpdateCycle", updateRateInSeconds, updateRateInSeconds);
         particleInstance1 = Instantiate(particlePrefab1, inhaleSpawnPoint.transform.position, Quaternion.identity);
         followPathRef = particleInstance1.GetComponentInChildren<FollowPath>();
-       /* particleInstance2 = Instantiate(particlePrefab2, inhaleSpawnPoint.transform.position, Quaternion.identity);
-        followPathRef2 = particleInstance2.GetComponentInChildren<FollowPath>();
-        particleInstance3 = Instantiate(particlePrefab3, inhaleSpawnPoint.transform.position, Quaternion.identity);
-        followPathRef3 = particleInstance3.GetComponentInChildren<FollowPath>();*/
         FullCircle(); // calls the function at the begining of the run
     }
 
     void Update()
     {
-
-        BoxCollider[] bc = GetComponentsInChildren<BoxCollider>();
-        timeNow = Time.realtimeSinceStartup; // sets timne to the runtime of the programm
-
+        timeSinceLaunch = Time.realtimeSinceStartup; // sets timne to the runtime of the programm
 
         UpdateSun();
         //currentTimeOfDay += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
@@ -95,31 +53,6 @@ public class daynight_cycle : MonoBehaviour
         }
 
         sun.color = sunset.Evaluate(currentTimeOfDay);
-/*
-        if (currentTimeOfDay >= 0.65)
-            {
-                sun.color = Color.Lerp(startColor, endColor, currentTimeOfDay);
-            }
-
-        else if (currentTimeOfDay >= 0.62)
-        {
-            sun.color = Color.Lerp(redColor, pinkColor, currentTimeOfDay);
-        }
-        else if (currentTimeOfDay >= 0.6)
-        {
-            sun.color = Color.Lerp(orangeColor, redColor, currentTimeOfDay);
-        }
-        else if (currentTimeOfDay >= 0.57)
-        {
-            sun.color = Color.Lerp(lightorangeColor, orangeColor, currentTimeOfDay);
-        }
-
-        else if (currentTimeOfDay >= 0.55)
-        {
-            sun.color = Color.Lerp(startColor, lightorangeColor, currentTimeOfDay);
-        }
-        */
-
     }
 
     void UpdateSun()
@@ -145,61 +78,18 @@ public class daynight_cycle : MonoBehaviour
         sun.intensity = sunInitialIntensity * intensityMultiplier;
     }
 
-    // void AdjustSunColor()
-    // {
-    //   sun.color = sunColor.Evaluate(sun.intensity);
-    //}
-
-    /*
- void UpdateFX()
- {
-     float tRange = 1 - minPoint;
-     float dot = Mathf.Clamp01((Vector3.Dot(sun.transform.forward, Vector3.down) - minPoint) / tRange);
-     float i = ((maxIntensity - minIntensity) * dot) + minIntensity;
-     sun.intensity = i;
-     if (moon != null)
-         moon.intensity = 1 - i;
-
-     i = ((maxBounceIntensity - minBounceIntensity) * dot) + minBounceIntensity;
-     sun.bounceIntensity = i;
-
-     tRange = 1 - minAmbientPoint;
-     dot = Mathf.Clamp01((Vector3.Dot(sun.transform.forward, Vector3.down) - minAmbientPoint) / tRange);
-     i = ((maxAmbient - minAmbient) * dot) + minAmbient;
-     RenderSettings.ambientIntensity = i;
-
-     sun.color = nightDayColor.Evaluate(dot);
-     RenderSettings.ambientLight = sun.color;
-
-     RenderSettings.fogColor = nightDayFogColor.Evaluate(dot);
-     RenderSettings.fogDensity = fogDensityCurve.Evaluate(dot) * fogScale;
-
-     i = ((dayAtmosphereThickness - nightAtmosphereThickness) * dot) + nightAtmosphereThickness;
-     skyMat.SetFloat("_AtmosphereThickness", i);
-     skyMat.SetFloat("_Exposure", i * exposureMultiplier);
- }
-
- */
-
     IEnumerator Timer()
     {
         while (true)
         {
-            time1 = 2 + (1 * (timeNow / (secondsInFullDay/4)));// updates the time for the graduale increase of the inhale cycle
-            time2 = 4 + (3 * (timeNow / (secondsInFullDay / 4)));// updates the time for the graduale increase of the exhale cycle
-            timeDelay = 0.3f + (0.3f * (timeNow / (secondsInFullDay / 4)));// updates delay between cycles
+            inhaleTime = 2 + (timeSinceLaunch / (secondsInFullDay/4)); //Gradually updates the time towards 3, based off of how close time/timeTotal is to 1
+            exhaleTime = 4 + (3 * (timeSinceLaunch / (secondsInFullDay / 4))); //Gradually updates the time towards 7, based off of how close time/timeTotal is to 1
+            breathDelay = 0.3f + (0.3f * (timeSinceLaunch / (secondsInFullDay / 4))); //Gradually updates the time towards .6, based off of how close time/timeTotal is to 1
 
+            followPathRef.TimeTotal1 = inhaleTime;
+            followPathRef.TimeTotal2 = exhaleTime;
 
-            followPathRef.TimeTotal1 = time1;
-            followPathRef.TimeTotal2 = time2;
-
-            //followPathRef2.TimeTotal1 = time1;
-            //followPathRef2.TimeTotal2 = time2;
-
-            //followPathRef3.TimeTotal1 = time1;
-            //followPathRef3.TimeTotal2 = time2;
-
-            yield return new WaitForSeconds(time1 +time2+ timeDelay);
+            yield return new WaitForSeconds(inhaleTime + breathDelay + exhaleTime + breathDelay);
         }
     }
 }

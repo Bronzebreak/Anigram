f264fcb2d4ea935456f617e35f17e4cf5c12240c
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-12-17 22:31:42 -0800

    no message

--- Assets/Scripts/daynight_cycle.cs
+++ Assets/Scripts/daynight_cycle.cs
@@ -7,10 +7,6 @@ using Liminal.Core.Fader;
 
 public class daynight_cycle : MonoBehaviour
 {
-    public AudioClip inhaleSound;
-    public AudioClip exhaleSound;
-    bool keepFadingIn;
-    bool keepFadingOut;
     public AudioSource InhaleSource;
     public AudioSource ExhaleSource;
     public float inhaleTime = 2f; // initial time of the inhale cycle
@@ -39,49 +35,35 @@ public class daynight_cycle : MonoBehaviour
 
     void Start()
     {
-        InhaleSource.clip = inhaleSound;
         sunInitialIntensity = sun.intensity;
         FullCircle(); // calls the function at the begining of the run
     }
 
     void Update()
     {
-        if (currentTimeOfDay >= .73f && !ending)
-        {
-            ending = true;
-            EndItAll();
-        }
-
-        //print(InhaleSource.volume);
-        if (!InhaleSource.isPlaying)
-        {
-            print("End");
-            //InhaleSource.volume = 0.9f;
-        }
-        if (!ExhaleSource.isPlaying)
-        {
-
-            print("End");
-            //ExhaleSource.volume = 0.9f;
-        }
         timeSinceLaunch = Time.realtimeSinceStartup; // sets timne to the runtime of the programm
-        //print(breathingSource.volume);
         UpdateSun();
-        //currentTimeOfDay += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
         currentTimeOfDay = (0.45f + (Time.realtimeSinceStartup / secondsInFullDay)) * timeMultiplier;
 
         if (currentTimeOfDay >= 1)
         {
             currentTimeOfDay -= 1;
         }
-        //breathingSource.pitch = (1.1f - (timeSinceLaunch / (secondsInFullDay ))) ;
+
         sun.color = sunset.Evaluate(currentTimeOfDay);
+
         if (RenderSettings.skybox.HasProperty("_Tint"))
         {
             RenderSettings.skybox.SetColor("_Tint", skybox.Evaluate(currentTimeOfDay));
             RenderSettings.skybox.SetFloat("_Exposure", .4f);
             RenderSettings.skybox.SetFloat("_Rotation", -300*currentTimeOfDay);
             RenderSettings.fogColor = fog.Evaluate(currentTimeOfDay);
+        }
+
+        if (currentTimeOfDay >= .73f && !ending)
+        {
+            ending = true;
+            EndItAll();
         }
     }
 
@@ -112,59 +94,27 @@ public class daynight_cycle : MonoBehaviour
     {
         while (true)
         {
-            StartCoroutine(StartFadeInhale(InhaleSource, inhaleTime + 0.1f, 0.0f));
-
-            yield return new WaitForSeconds(inhaleTime + breathDelay);
-            StartCoroutine(StartFadeInhale(ExhaleSource, exhaleTime + 0.1f, 0.0f));
-            yield return new WaitForSeconds(exhaleTime + breathDelay);
+            StartCoroutine(VolumeFader(InhaleSource, inhaleTime + 0.1f, 0.0f));// starts the couroutine
+            yield return new WaitForSeconds(inhaleTime + breathDelay); // Wait For the end of the coroutinea by delay
+            StartCoroutine(VolumeFader(ExhaleSource, exhaleTime + 0.1f, 0.0f));// starts the coroutine withe different audiosource and time.
+            yield return new WaitForSeconds(exhaleTime + breathDelay);// delay 
             //After breathing cycle...
             inhaleTime = 2 + (timeSinceLaunch / (300)); //Gradually updates the time towards 3, based off of how close time/timeTotal is to 1
             exhaleTime = 4 + (3 * (timeSinceLaunch / (300))); //Gradually updates the time towards 7, based off of how close time/timeTotal is to 1
             breathDelay = 0.3f + (0.3f * (timeSinceLaunch / 300)); //Gradually updates the time towards .6, based off of how close time/timeTotal is to 1
         }
     }
-    /*
 
-        IEnumerator FadeIN(AudioSource breath, float speed, float maxVolume)
-        {
-            keepFadingIn = true;
-            keepFadingOut = false;
-            breath.volume = 0;
-            float AudioVolume = breath.volume;
-            while (breath.volume< maxVolume && keepFadingIn)
-            {
-                AudioVolume += speed;
-                breath.volume = AudioVolume;
-                yield return new WaitForSeconds(0.1f);
-            }
-        }
-        IEnumerator FadeOUT(AudioSource breath, float speed)
-        {
-            keepFadingIn = false;
-            keepFadingOut = true;
-
-            float AudioVolume = breath.volume;
-            while (breath.volume >= speed && keepFadingOut)
-            {
-                AudioVolume -= speed;
-                breath.volume = AudioVolume;
-                yield return new WaitForSeconds(0.1f);
-            }
-        }
-        */
-    public IEnumerator StartFadeInhale(AudioSource audioSource, float duration, float targetVolume)
+    public IEnumerator VolumeFader(AudioSource audioSource, float duration, float targetVolume)
     {
-        audioSource.volume = 0.8f;
-        //yield return new WaitForSeconds(0.1f);
-        audioSource.Play();
-
+        audioSource.volume = 1f;// sets audio source volume value
+        audioSource.Play();// starts the audio source
         float currentTime = 0;
-        float start = audioSource.volume;
-
+        float start = audioSource.volume; // sets the initial value for lerp
         while (currentTime < duration)
         {
             currentTime += Time.deltaTime;
-            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            audioSource.volume = Mathf.SmoothStep(start, targetVolume, currentTime / duration);// lerps the volume between intial value and zero
             yield return null;
         }
         yield break;
